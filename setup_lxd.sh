0fc08f6769ffbead4fd8875a66a2a557d5da7e9f
Alexandros Kosiaris <akosiaris@gmail.com>
2018-07-05 16:42:41 +0300

    Add proxy to /etc/wgetrc if defined
    deboostrap relies on /etc/wgetrc instead of env variables so populate
    that.

--- setup_lxd.sh
+++ setup_lxd.sh
@@ -1,5 +1,7 @@
 if [ -n "${HTTP_PROXY}" ]; then
 	echo "Acquire::http::Proxy \"http://${HTTP_PROXY}\";" >> /etc/apt/apt.conf.d/80proxy
+	echo "http_proxy = http://${HTTP_PROXY}" >> /etc/wgetrc
+	echo "https_proxy = http://${HTTP_PROXY}" >> /etc/wgetrc
 fi
 apt-get update
 apt-get install -y lxd lxd-client puppet
