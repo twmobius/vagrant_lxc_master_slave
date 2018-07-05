6c374f4bcd80662685baf24fe49982f8ebb7043c
Alexandros Kosiaris <akosiaris@gmail.com>
2018-07-05 19:39:05 +0300

    Retab lxc::master, lxc::slave files
    Remove all tab characters and replace them with 4 spaces

--- puppet/modules/role/manifests/lxc/slave.pp
+++ puppet/modules/role/manifests/lxc/slave.pp
@@ -20,59 +20,58 @@ class role::lxc::slave(
         lxc_cgmanager_service_ensure  => false,
         lxc_cgmanager_service_enabled => false,
     }
-	$lxc_defaults = {
+
+    $lxc_defaults = {
         ensure          => 'present',
         template        => 'ubuntu',
         storage_backend => 'dir',
-		require 		=> Rsync::Get['lxc'],
-	}
+        require         => Rsync::Get['lxc'],
+    }
+    $lxcs = {
+        'db-1' => { state => 'stopped', autostart => false, },
+        'db-2' => { state => 'running', autostart => true, },
+        'web-1' => { state => 'stopped', autostart => false, },
+        'web-2' => { state => 'running', autostart => true, },
+        'ad-1' => { state => 'stopped', autostart => false, },
+        'dns-1' => { state => 'stopped', autostart => false, },
+        'queue-1' => { state => 'stopped', autostart => false, },
+        'search-1' => { state => 'stopped', autostart => false, },
+        'live-1' => { state => 'stopped', autostart => false, },
+        'logs-1' => { state => 'stopped', autostart => false, },
+        'vpn-1' => { state => 'stopped', autostart => false, },
+        'mail-1' => { state => 'stopped', autostart => false, },
+        'mc-1' => { state => 'stopped', autostart => false, },
+        'various-1' => { state => 'stopped', autostart => false, },
+        'redis-1' => { state => 'stopped', autostart => false, },
+        'zabbix-1' => { state => 'stopped', autostart => false, },
+    }
+    create_resources(lxc, $lxcs, $lxc_defaults)
 
-	$lxcs = {
-		'db-1' => { state => 'stopped', autostart => false, },
-		'db-2' => { state => 'running', autostart => true, },
-		'web-1' => { state => 'stopped', autostart => false, },
-		'web-2' => { state => 'running', autostart => true, },
-		'ad-1' => { state => 'stopped', autostart => false, },
-		'dns-1' => { state => 'stopped', autostart => false, },
-		'queue-1' => { state => 'stopped', autostart => false, },
-		'search-1' => { state => 'stopped', autostart => false, },
-		'live-1' => { state => 'stopped', autostart => false, },
-		'logs-1' => { state => 'stopped', autostart => false, },
-		'vpn-1' => { state => 'stopped', autostart => false, },
-		'mail-1' => { state => 'stopped', autostart => false, },
-		'mc-1' => { state => 'stopped', autostart => false, },
-		'various-1' => { state => 'stopped', autostart => false, },
-		'redis-1' => { state => 'stopped', autostart => false, },
-		'zabbix-1' => { state => 'stopped', autostart => false, },
-	}
-
-	create_resources(lxc, $lxcs, $lxc_defaults)
-
-	$lxc_interface_defaults = {
-		device_name => 'eth0',
-		ensure      => present,
-		index       => 0,
-		link        => 'lxcbr0',
-		type        => 'veth',
-		restart     => true,
-	}
+    $lxc_interface_defaults = {
+        device_name => 'eth0',
+        ensure      => present,
+        index       => 0,
+        link        => 'lxcbr0',
+        type        => 'veth',
+        restart     => true,
+    }
     $lxc_interfaces = {
-		'db-1' => { container => 'db-1', ipv4 => ['10.1.1.10/24'], },
-		'db-2' => { container => 'db-2', ipv4 => ['10.1.1.11/24'], },
+        'db-1' => { container => 'db-1', ipv4 => ['10.1.1.10/24'], },
+        'db-2' => { container => 'db-2', ipv4 => ['10.1.1.11/24'], },
         'web-1' => { container => 'web-1', ipv4 => ['10.1.1.20/24'], },
         'web-2' => { container => 'web-2', ipv4 => ['10.1.1.21/24'], },
-		'ad-1' => { container => 'ad-1', ipv4 => ['10.1.1.30/24'], },
-		'dns-1' => { container => 'dns-1', type => 'none' }, # This is on the host namespace
-		'queue-1' => { container => 'queue-1', ipv4 => ['10.1.1.40/24'],
-		}, 'search-1' => { container => 'search-1', ipv4 => ['10.1.1.50/24'], },
-		'live-1' => { container => 'live-1', ipv4 => ['10.1.1.60/24'], },
-		'logs-1' => { container => 'logs-1', ipv4 => ['10.1.1.70/24'], },
-		'vpn-1'  => { container => 'vpn-1', type => 'none' }, # On the host namespace
-		'mail-1' => { container => 'mail-1', ipv4 => ['10.1.1.80/24'], },
-		'mc-1' => { container => 'mc-1', ipv4 => ['10.1.1.90/24'], },
-		'various-1' => { container => 'various-1', ipv4 => ['10.1.1.100/24'], },
-		'redis-1' => { container => 'redis-1', ipv4 => ['10.1.1.110/24'], },
-		'zabbix-1' => { container => 'redis-1', type => 'none' }, # On the host namespace
+        'ad-1' => { container => 'ad-1', ipv4 => ['10.1.1.30/24'], },
+        'dns-1' => { container => 'dns-1', type => 'none' }, # This is on the host namespace
+        'queue-1' => { container => 'queue-1', ipv4 => ['10.1.1.40/24'], },
+        'search-1' => { container => 'search-1', ipv4 => ['10.1.1.50/24'], },
+        'live-1' => { container => 'live-1', ipv4 => ['10.1.1.60/24'], },
+        'logs-1' => { container => 'logs-1', ipv4 => ['10.1.1.70/24'], },
+        'vpn-1'  => { container => 'vpn-1', type => 'none' }, # On the host namespace
+        'mail-1' => { container => 'mail-1', ipv4 => ['10.1.1.80/24'], },
+        'mc-1' => { container => 'mc-1', ipv4 => ['10.1.1.90/24'], },
+        'various-1' => { container => 'various-1', ipv4 => ['10.1.1.100/24'], },
+        'redis-1' => { container => 'redis-1', ipv4 => ['10.1.1.110/24'], },
+        'zabbix-1' => { container => 'redis-1', type => 'none' }, # On the host namespace
     }
-	create_resources(lxc_interface, $lxc_interfaces, $lxc_interface_defaults)
+    create_resources(lxc_interface, $lxc_interfaces, $lxc_interface_defaults)
 }
