e6d7c470c6816e8a708c09887df0287a7b895b19
mobius <paris@forin.gr>
2018-07-13 10:54:18 +0200

    Changes the comments

--- puppet/modules/firehol/manifests/ipv6.pp
+++ puppet/modules/firehol/manifests/ipv6.pp
@@ -1,7 +1,5 @@
 # Resource for a FireHOL Interface
 #
-# @param variable Name of the variable, e.g. FIREHOL_ENABLE_SPINNER.
-# @param value The value of the variable
 # @param config_file String param, which config file to work with. Defaults to '/etc/firehol/firehol.con'.
 define firehol::ipv6 (
   String                           $config_file    = '/etc/firehol/firehol.conf',
