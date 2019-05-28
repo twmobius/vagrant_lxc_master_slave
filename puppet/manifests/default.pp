1302ee292985401ff84c5489c1fccf32836e9b66
mobius <paris@forin.gr>
2019-05-28 09:16:14 +0200

    Changes the notifications lxc name

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -42,5 +42,5 @@ $lxcs = {
     'ai-1'          => { ipv4 => '10.1.1.140', on_master => false },
     'workspaces-1'  => { ipv4 => '10.1.1.150', on_master => false },
     'analytics'     => { ipv4 => '10.1.1.160', on_master => false },
-    'notifications-1'     => { ipv4 => '10.1.1.170', on_master => false }
+    'contact-1'     => { ipv4 => '10.1.1.170', on_master => false }
 }
